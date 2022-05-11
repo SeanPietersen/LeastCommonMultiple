2a2c603776c0475c6af21e67ead8a33d960ef2a7
Sean Pietersen <seanpietersen7@gmail.com>
2022-05-11 15:32:05 +0200

    Feat: Added App

---
+++ LeastCommonMultiple/LeastCommonMultiple.App/CalculateTheLeastCommonMultiple.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeastCommonMultiple.App
+{
+    public class CalculateTheLeastCommonMultiple : ICalculateTheLeastCommonMultiple
+    {
+        public int gcdCalculator(int num1, int num2)
+        {
+            if(num2 == 0)
+            {
+                return num1;
+            }
+            else
+            {
+                return gcdCalculator(num2, num1 % num2);
+            }
+        }
+
+        public int LowestCommonMultiple(List<int> nums)
+        {
+            return nums.Aggregate((S, Val) => S * Val / gcdCalculator(S, Val));
+        }
+    }
+}
--- LeastCommonMultiple/LeastCommonMultiple.Test/CalculateTheLeastCommonMultipleTest.cs
+++ LeastCommonMultiple/LeastCommonMultiple.Test/CalculateTheLeastCommonMultipleTest.cs
@@ -1,4 +1,6 @@
+using LeastCommonMultiple.App;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace LeastCommonMultiple.Test
@@ -6,9 +8,48 @@ namespace LeastCommonMultiple.Test
     public class CalculateTheLeastCommonMultipleTest
     {
         [Fact]
-        public void LowestCommonMultipleTest1()
+        public void LowestCommonMultipleCalculatorTest1()
         {
+            //Arrange
+            ICalculateTheLeastCommonMultiple calculateTheLeastCommonMultiple = new CalculateTheLeastCommonMultiple();
+            var list = new List<int> { 2, 5 };
+            var expected = 10;
 
+            //Act
+            var actual = calculateTheLeastCommonMultiple.LowestCommonMultiple(list);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LowestCommonMultipleCalculatorTest2()
+        {
+            //Arrange
+            ICalculateTheLeastCommonMultiple calculateTheLeastCommonMultiple = new CalculateTheLeastCommonMultiple();
+            var list = new List<int> { 2, 3, 4 };
+            var expected = 12;
+
+            //Act
+            var actual = calculateTheLeastCommonMultiple.LowestCommonMultiple(list);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void LowestCommonMultipleCalculatorTest3()
+        {
+            //Arrange
+            ICalculateTheLeastCommonMultiple calculateTheLeastCommonMultiple = new CalculateTheLeastCommonMultiple();
+            var list = new List<int> { 9};
+            var expected = 9;
+
+            //Act
+            var actual = calculateTheLeastCommonMultiple.LowestCommonMultiple(list);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
